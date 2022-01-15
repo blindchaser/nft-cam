4f9a351a70038d5b3764ad11c2a96af60e24d4b8
Yicheng Li <ycli@alumni.stanford.edu>
2022-01-14 20:45:12 -0800

    React + webpack setup

---
+++ README.md
@@ -0,0 +1,9 @@
+## Available Scripts
+
+### `npm install`
+
+Installs dependencies.
+
+### `npm run dev`
+
+Uses Webpack to build the app and serve in a local dev server.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+        Your Boilerplate App is running
+      </div>
+  )
+}
+
+export default App;
