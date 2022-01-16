0f38c99a810e5a04be1670128b54c0c4e281381a
Fred Chyan <fchyan@gmail.com>
2022-01-15 23:26:36 -0800

    use functional component for arcam

--- frontend/src/NFTList.js
+++ frontend/src/NFTList.js
@@ -1,51 +1,53 @@
-import React, { useContext, useEffect, useState } from "react";
-import Typography from "@mui/material/Typography";
-import { DataGrid } from "@mui/x-data-grid";
-import TryInAR from "./TryInAR";
-import { UserContext } from "./LoginContext";
-import ARCam from "./threejs/ARCam";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from 'react'
+import Typography from '@mui/material/Typography'
+import { DataGrid } from '@mui/x-data-grid'
+import TryInAR from './TryInAR'
+import { UserContext } from './LoginContext'
+import ARCam from './threejs/ARCam'
+import { Link } from 'react-router-dom'
 
 const columns = [
-  { field: "icon", headerName: "", width: 120 },
-  { field: "name", headerName: "NAME", width: 120 },
-  { field: "collection", headerName: "COLLECTION", width: 240 },
-  { field: "type", headerName: "TYPE", width: 120 },
+  { field: 'icon', headerName: '', width: 120 },
+  { field: 'name', headerName: 'NAME', width: 120 },
+  { field: 'collection', headerName: 'COLLECTION', width: 240 },
+  { field: 'type', headerName: 'TYPE', width: 120 },
   {
-    field: "showInAR",
-    headerName: "",
+    field: 'showInAR',
+    headerName: '',
     renderCell: (cellValues) => {
-      return <Link to="/arcam" style={{ color: "#ffd500" }}>Try in AR</Link>;
+      return (
+        <Link to="/arcam" style={{ color: '#ffd500' }}>
+          Try in AR
+        </Link>
+      )
     },
   },
-];
+]
 
 export default function NFTList() {
-  const [user] = useContext(UserContext);
-  let [rows, updateRows] = useState([]);
+  const [user] = useContext(UserContext)
+  let [rows, updateRows] = useState([])
   useEffect(() => {
-    {
-      rows = [];
-      if (user.nfts) {
-        for (let i = 0; i < user.nfts.length; i++) {
-          const info = user.nfts[i];
-          updateRows((r) => [
-            ...r,
-            {
-              id: i,
-              name: info.name,
-              collection: info.asset_contract.name,
-              type: info.animation_original_url ? "Animation" : "Still",
-            },
-          ]);
-        }
-      } else {
-        console.log("user.nfts is undefined");
+    rows = []
+    if (user.nfts) {
+      for (let i = 0; i < user.nfts.length; i++) {
+        const info = user.nfts[i]
+        updateRows((r) => [
+          ...r,
+          {
+            id: i,
+            name: info.name,
+            collection: info.asset_contract.name,
+            type: info.animation_original_url ? 'Animation' : 'Still',
+          },
+        ])
       }
+    } else {
+      console.log('user.nfts is undefined')
     }
-  }, []);
+  }, [])
   return (
-    <div style={{ height: 400, width: "60%" }}>
+    <div style={{ height: 400, width: '60%' }}>
       <Typography variant="h3" align="center" color="white">
         NFT Cam
       </Typography>
@@ -54,8 +56,8 @@ export default function NFTList() {
         columns={columns}
         pageSize={10}
         rowsPerPageOptions={[10]}
-        sx={{ color: "white", border: "0px", marginTop: "50px" }}
+        sx={{ color: 'white', border: '0px', marginTop: '50px' }}
       />
     </div>
-  );
+  )
 }
--- frontend/src/threejs/ARCam.js
+++ frontend/src/threejs/ARCam.js
@@ -1,27 +1,37 @@
-import React, { Component } from "react";
-import initScene from "./scene";
-import "./scene.scss";
-import 'regenerator-runtime/runtime';
+import React, { Component, useContext, useEffect, useState } from 'react'
+import initScene from './scene'
+import './scene.scss'
+import 'regenerator-runtime/runtime'
+import { UserContext } from '../LoginContext'
 
-class ARCam extends Component {
-  componentDidMount = async () => {
-    initScene();
-  };
+// class ARCam extends Component {
+//   componentDidMount = async () => {
+//     initScene()
+//   }
 
-  render() {
-    return (
-      <div className="container">
-        <canvas className="output_canvas"></canvas>
-        <canvas
-          id="threeDCanvas"
-        ></canvas>
-        <video
-          id="webcam_video"
-          className="input_video"
-        ></video>
-      </div>
-    );
-  }
+//   render() {
+//     const [user] = useContext(UserContext)
+//     return (
+//       <div className="container">
+//         <canvas className="output_canvas"></canvas>
+//         <canvas id="threeDCanvas"></canvas>
+//         <video id="webcam_video" className="input_video"></video>
+//       </div>
+//     )
+//   }
+// }
+
+// export default ARCam
+
+export default function ARCam() {
+  useEffect(() => {
+    initScene()
+  }, [])
+  return (
+    <div className="container">
+      <canvas className="output_canvas"></canvas>
+      <canvas id="threeDCanvas"></canvas>
+      <video id="webcam_video" className="input_video"></video>
+    </div>
+  )
 }
-
-export default ARCam;
